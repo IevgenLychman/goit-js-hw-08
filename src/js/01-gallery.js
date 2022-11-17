97a3af14821366f07bec24c972b756ba92c9ee7a
IevgenLychman <gora.erikovich@gmail.com>
2022-11-17 11:32:15 +0200

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,27 @@
+// Add imports above this line
+import SimpleLightbox from 'simpleLightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items';
+
+// Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkupTemplate = createImageEl(galleryItems);
+
+function createImageEl(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <li>
+      <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+      </li>`;
+    })
+    .join('');
+}
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkupTemplate);
+
+const gallery = new SimpleLightbox('.gallery a');
+
+console.log(galleryItems);
