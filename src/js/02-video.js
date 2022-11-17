55bbb427d1e8befa6b35b561340fe82f69d741e0
IevgenLychman <gora.erikovich@gmail.com>
2022-11-17 15:26:49 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,10 +1,20 @@
+import vimeoPlayer from '@vimeo/player';
+
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new vimeoPlayer(iframe);
 
 player.on('play', function () {
+  localStorage.setItem('duration', time);
+  console.log(player.timeupdate);
   console.log('played the video!');
 });
 
 player.getVideoTitle().then(function (title) {
   console.log('title:', title);
 });
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
